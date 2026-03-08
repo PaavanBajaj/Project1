1589bcddc5d58a28f490ea1f9017b53a5b5d1092
Paavan Bajaj <paavanbajaj@Paavans-MacBook-Pro.local>
2026-03-07 16:52:24 -0800

    Auto push: 16:52:24 | Changes:  M auto_push.sh

--- auto_push.sh
+++ auto_push.sh
@@ -2,8 +2,9 @@
 
 while true; do
   if [[ -n $(git status --porcelain) ]]; then
+    CHANGED=$(git status -s)          
     git add .
-    git commit -m "auto update"
+    git commit -m "Auto push: $(date '+%H:%M:%S') | Changes: $CHANGED"
     git push
   fi
   sleep 300
